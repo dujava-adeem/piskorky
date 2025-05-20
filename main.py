2a6fe974955d2d775a4fb06755341396e220ef7a
adam <adamdujava@gmail.com>
2025-05-20 12:21:48 +0200

    serus 1

---
+++ main.py
@@ -0,0 +1,27 @@
+def nakresli_riadok(sirka,index_riadku):
+    print(index_riadku,end=" ")
+    for i in range(sirka):
+        nakresli_pole()
+
+def nakresli_pole():
+    print("*",end=" ")
+
+def nakresli_cele_pole():
+    nakresli_1_riadok(sirka)
+    for j in range(vyska):
+        nakresli_riadok(sirka,j)
+        print("")
+def nakresli_1_riadok(sirka):
+    print(" ",end=" ")
+    for i in range(sirka):
+        print(i, end = " ")
+    print("")
+
+vyska=int(input("zadaj cislo: "))
+sirka=int(input("zadaj cislo: "))
+nakresli_cele_pole()
+
+"""for i in range(s):
+    for j in range(v):
+        print("+-", end=" ")
+    print("I  I")!"""
