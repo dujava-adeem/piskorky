068eaf266b0b5c96cb9042d3f6df00407211e727
dominikdujava <dujavatech@gmail.com>
2025-05-21 12:51:29 +0200

    Opraveny styl, odstranena medzera

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
-def nakresli_riadok(sirka,index_riadku):
-    print(index_riadku,end="")
+def nakresli_riadok(sirka, index_riadku):
+    print(index_riadku, end="")
     for i in range(sirka):
-        nakresli_pole(index_riadku,i)
-    print("|",end="")
+        nakresli_pole(index_riadku, i)
+    print("|", end="")
 
-def nakresli_pole(vyska_1,sirka_1):
-    print("|",hra[vyska_1][sirka_1],end="")
+
+def nakresli_pole(vyska_1, sirka_1):
+    print("|", hra[vyska_1][sirka_1], sep="", end="")
+
 
 def nakresli_cele_pole():
     nakresli_1_riadok(sirka)
     for j in range(vyska):
         plusminus(sirka)
-        nakresli_riadok(sirka,j)
+        nakresli_riadok(sirka, j)
         print("")
     plusminus(sirka)
+
+
 def nakresli_1_riadok(sirka):
-    print(" ",end=" ")
+    print("  ", end="")
     for i in range(sirka):
-        print(i, end = " ")
+        print(i, end=" ")
     print("")
 
+
 def plusminus(sirka):
-    print("", end=" ")
+    print(" ", end="")
     for i in range(sirka):
-        print("+-",end="")
+        print("+-", end="")
     print("+")
 
-def herna():
+
+def vypln_hracie_pole():
+    for i in range(vyska):
+        hra.append([])
+        for j in range(sirka):
+            hra[i].append(" ")
+
+
+def spustit_hru():
     while True:
-        vyska_1=int(input("zadaj cislo(v): "))
-        sirka_1 =int(input("zadaj cislo(s): "))
-        hra[vyska_1][sirka_1]= "X"
+        vyska_1 = int(input("zadaj cislo(v): "))
+        sirka_1 = int(input("zadaj cislo(s): "))
+        hra[vyska_1][sirka_1] = "X"
         nakresli_cele_pole()
 
-vyska=int(input("zadaj cislo: "))
-sirka=int(input("zadaj cislo: "))
-hra=[ [] ]
-for i in range(vyska):
-    hra.append([])
-    for j in range(sirka):
-        hra[i].append(" ")
 
+vyska = int(input("zadaj cislo: "))
+sirka = int(input("zadaj cislo: "))
 
+hra = [[]]
+vypln_hracie_pole()
 
-herna()
+spustit_hru()
 
-# riadok má 2 časti:
-# - horny oddelovac     +-+-+-+-+
-# - policko so znakom   |*|*|*|*|
+# Úloha 1:
+# - doplniť kontrolu vstupu - či je to číslo
+# - dolpniť kontrolu pri ťahu - či je to menšie a ako hracie pole
+# - doplniť kontrolu, či už je policku vyplnené
 
-# policko má 2 časti:
-# - horny oddelovac     +-
-# - policko so znakom   |*
+# Úloha 2 (Využiť OOP - hrac ako classa - meno a symbol):
+# - doplniť striedanie hráčov
+#     - striedanie hráč A (má znak X) a hráč B (má znak O)
+#     - upravené výpisy
